3487ef3f65daf8b711182a394c1ab3b80cd9917a
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-20 09:48:44 -0500

    reafactor(backend) connect route to controller

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,20 +1,8 @@
 const { Router } = require('express');
-const { pool }   = require('../db/config');
+const { newTransaction } = require('../controllers/transactions');
 
 const router = Router();
 
-router.post('/', async(req, res) => {
-    const { concept, amount, date, category, type} = req.body;
-
-    try {
-        const newTransaction = await pool.query(
-            "INSERT INTO transactions (concept, amount, date, category, type) " +
-            "VALUES($1, $2, $3, $4, $5) RETURNING *", [concept, amount, date, category, type]
-        )
-        res.status(201).json({ ...newTransaction.rows[0] });
-    } catch (error) {
-        console.log(error)
-    }
-})
+router.post('/', newTransaction);
 
 module.exports = router;
