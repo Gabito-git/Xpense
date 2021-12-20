bfd889f1ece71038eb5ed90ab4a1c2e17f74d326
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-20 10:26:54 -0500

    feat(backend) configure validate fields middleware
    configure middleware to throw custom error structure)

--- backend/middlewares/validateFields.js
+++ backend/middlewares/validateFields.js
@@ -1,10 +1,11 @@
 const { validationResult } = require('express-validator');
+const RequestValidationError = require('../errors/request-validation-err');
 
 const validateFields = (req, res, next) => {
     const errors = validationResult(req);
     
     if(!errors.isEmpty()){
-        return res.status(400).json(errors);
+       throw new RequestValidationError(errors.array());
     }
 
     next();
