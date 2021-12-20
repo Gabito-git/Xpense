42b777c353fefafe23a2a21aa3f3ad0232afc440
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-20 18:24:08 -0500

    feat(web) add Button componen mark up

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ className, text }) => {
+    return (
+        <button className={ `button ${className}` }>
+            { text }
+        </button>
+    )
+}
+
+export default Button
