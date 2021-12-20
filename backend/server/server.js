7f5c54a65e10ef85fbdd3647e5f3631120b625fc
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-20 09:10:59 -0500

    feat(backend) executes middlewares
    executes bodyparser express and cors middlewares

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const cors    = require('cors');
 require('dotenv').config();
 
 class Server{
 
     constructor(){
-
         this.app = express();
         this.port = process.env.PORT || 4000;
 
+        this.middlewares();
+
         this.execute();
+    }
+
+    middlewares(){
+        this.app.use( express.json() );
+        this.app.use(cors());
     }
 
     execute(){
