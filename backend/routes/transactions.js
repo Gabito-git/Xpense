c4f772c20aa1082daaeae698edefde42d7ab04b9
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-20 10:05:06 -0500

    feat(backend) implement field validations in POST route

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
+const { check }  = require('express-validator');
+ 
 const { newTransaction } = require('../controllers/transactions');
+const { allowedTypes }   = require('../helpers/db-validators');
+const validateFields     = require('../middlewares/validateFields');
 
 const router = Router();
 
-router.post('/', newTransaction);
+router.post('/',[
+    check('concept', 'Concept field is mandatory').not().isEmpty(),
+    check('amount', 'Please provide a valid amount value').isFloat(),
+    check('date', 'Please provide a valid date').isISO8601(),
+    check('category', 'Category field is mandatory').not().isEmpty(),
+    check('type').custom( c => allowedTypes( c, ['income', 'expense'] ) ),
+    validateFields
+ ], newTransaction);
 
 module.exports = router;
