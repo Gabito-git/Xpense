f5ef17b804a1eadf38f70901aef24127b501be46
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-20 09:43:24 -0500

    feat(backend) add new transaction feature

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,11 +1,20 @@
 const { Router } = require('express');
+const { pool }   = require('../db/config');
 
 const router = Router();
 
-router.post('/', (req, res) => {
-    console.log('transaction posted')
+router.post('/', async(req, res) => {
+    const { concept, amount, date, category, type} = req.body;
+
+    try {
+        const newTransaction = await pool.query(
+            "INSERT INTO transactions (concept, amount, date, category, type) " +
+            "VALUES($1, $2, $3, $4, $5) RETURNING *", [concept, amount, date, category, type]
+        )
+        res.status(201).json({ ...newTransaction.rows[0] });
+    } catch (error) {
+        console.log(error)
+    }
 })
-
-
 
 module.exports = router;
