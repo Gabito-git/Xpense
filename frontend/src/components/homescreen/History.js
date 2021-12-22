8ca5f170e690aa9d7436ab6e36d7557a3e0bb0f3
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 18:50:20 -0500

    refactor(front) include search by category field

--- frontend/src/components/homescreen/History.js
+++ frontend/src/components/homescreen/History.js
@@ -1,9 +1,25 @@
+import { useState } from "react"
+import CustomSelect from "./CustomSelect"
 import Transaction from "./Transaction"
 
 const History = () => {
+
+    const [selectedOption, setSelectedOption] = useState(null);
+
     return (
         <div className="history">
-            <h2 className="history__title">History</h2>
+            <div>
+                <h2 className="history__title">History</h2>
+                <div className="history__search"> 
+                    <p className="history__text">Search by category: </p>
+                    <div className="history__select-field">
+                        <CustomSelect 
+                            selectedOption={ selectedOption }
+                            setSelectedOption={ setSelectedOption }
+                        />
+                    </div>                    
+                </div>                
+            </div>
             <Transaction />
         </div>
     )
