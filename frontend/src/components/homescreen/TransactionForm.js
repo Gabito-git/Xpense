aa60e522a4519ba9004891ba26b77c5f8083aa57
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 11:54:43 -0500

    refactor(front) add customselect component to transaction form

--- frontend/src/components/homescreen/TransactionForm.js
+++ frontend/src/components/homescreen/TransactionForm.js
@@ -1,40 +1,14 @@
 import { useState } from "react";
 
-import Select from 'react-select';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 import  Button from '../../components/Button';
-
-const options = [
-    { value: 'salary', label: 'Salary' },
-    { value: 'interests', label: 'Interests' },
-    { value: 'food', label: 'Food' },
-    { value: 'transportation', label: 'Transportation' },
-    { value: 'gift', label: 'Gift' },
-    { value: 'family', label: 'Family' },
-    { value: 'other', label: 'Other' },
-];
-
-const customStyles = {   
-    option: (styles, {  isFocused }) => {
-      return {
-        ...styles,            
-        color: isFocused ? 'white': 'black',  
-      };
-    },
-    control: (styles) => ({
-        ...styles,
-        backgroundColor: 'transparent',        
-    } )  
-    
-};
+import CustomSelect from "./CustomSelect";
 
 const TransactionForm = () => {
 
     const [transactionDate, setTransactionDate] = useState(new Date());
-    const [selectedOption, setSelectedOption] = useState(null);
-
 
     return (
         <div className="transaction-form">
@@ -67,23 +41,7 @@ const TransactionForm = () => {
                 <div className="transaction-form__group">
                     <label> Category </label>
                     <div className="transaction-form__date-div">
-                            <div className="new-transaction__select-div">
-                                        <Select  
-                                            defaultValue={selectedOption}
-                                            menuPlacement="top"
-                                            onChange={setSelectedOption}                     
-                                            options={ options }
-                                            styles={ customStyles }
-                                            theme={(theme) => ({
-                                                ...theme,                                
-                                                colors: {
-                                                ...theme.colors,
-                                                primary25: '#519259',                                  
-                                                primary: '#064635',
-                                                },
-                                            })}
-                                        />
-                            </div> 
+                        <CustomSelect /> 
                     </div>
                 </div>
 
@@ -92,8 +50,7 @@ const TransactionForm = () => {
                         text="Add transaction"
                         className="transaction-form__button"
                     />
-                </div>
-                
+                </div>                
             </form>
 
         </div>
