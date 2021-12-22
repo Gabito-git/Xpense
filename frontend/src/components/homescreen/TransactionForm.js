5564727a9ef8ea41aa4c828180648c8e2536284c
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 11:04:54 -0500

    feat(front) add select component
    add select category feature to transaction form

--- frontend/src/components/homescreen/TransactionForm.js
+++ frontend/src/components/homescreen/TransactionForm.js
@@ -1,13 +1,40 @@
 import { useState } from "react";
 
+import Select from 'react-select';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 import  Button from '../../components/Button';
 
+const options = [
+    { value: 'salary', label: 'Salary' },
+    { value: 'interests', label: 'Interests' },
+    { value: 'food', label: 'Food' },
+    { value: 'transportation', label: 'Transportation' },
+    { value: 'gift', label: 'Gift' },
+    { value: 'family', label: 'Family' },
+    { value: 'other', label: 'Other' },
+];
+
+const customStyles = {   
+    option: (styles, {  isFocused }) => {
+      return {
+        ...styles,            
+        color: isFocused ? 'white': 'black',  
+      };
+    },
+    control: (styles) => ({
+        ...styles,
+        backgroundColor: 'transparent',        
+    } )  
+    
+};
+
 const TransactionForm = () => {
 
     const [transactionDate, setTransactionDate] = useState(new Date());
+    const [selectedOption, setSelectedOption] = useState(null);
+
 
     return (
         <div className="transaction-form">
@@ -34,6 +61,29 @@ const TransactionForm = () => {
                             onChange={(date) => setTransactionDate(date)} 
                             className="transaction-form__date"
                         />
+                    </div>
+                </div>
+
+                <div className="transaction-form__group">
+                    <label> Category </label>
+                    <div className="transaction-form__date-div">
+                            <div className="new-transaction__select-div">
+                                        <Select  
+                                            defaultValue={selectedOption}
+                                            menuPlacement="top"
+                                            onChange={setSelectedOption}                     
+                                            options={ options }
+                                            styles={ customStyles }
+                                            theme={(theme) => ({
+                                                ...theme,                                
+                                                colors: {
+                                                ...theme.colors,
+                                                primary25: '#519259',                                  
+                                                primary: '#064635',
+                                                },
+                                            })}
+                                        />
+                            </div> 
                     </div>
                 </div>
 
