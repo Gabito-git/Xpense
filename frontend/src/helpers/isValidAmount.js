d529030e44bc029046b72df03b9c729c3662bfd5
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 18:37:33 -0500

    (front) add isValidAmount helper

---
+++ frontend/src/helpers/isValidAmount.js
@@ -0,0 +1,14 @@
+export const isValidAmount = ( amount ) => {
+    
+    if( isNaN(amount) || amount.trim().length === 0 ){
+        return {ok: false};
+    }
+
+    return {
+        ok: true,
+        concept: amount > 0 ? 'income': 'expense',
+        value: Math.abs(amount)
+    }
+    
+}
+
