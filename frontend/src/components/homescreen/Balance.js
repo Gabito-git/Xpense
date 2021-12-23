0cb41749527d5b0760f3e179c7f7b74a5f945412
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 20:15:13 -0500

    feat(front) apply context to balance component

--- frontend/src/components/homescreen/Balance.js
+++ frontend/src/components/homescreen/Balance.js
@@ -1,19 +1,24 @@
+import { useContext } from "react"
+import { TransactionContext } from "../../context/transactionContext"
+
 const Balance = () => {
+    const { state: { incomes, expenses, total } } = useContext(TransactionContext);
+
     return (
         <div className="balance">
             <div className="balance__top">
                 <div className="balance__trans-info balance__trans-info--income">
                     <h2>INCOMES</h2>
-                    <p>USD<span> 2500</span></p>
+                    <p>USD<span> { incomes }</span></p>
                 </div>
                 <div className="balance__trans-info balance__trans-info--expense">
                     <h2>EXPENSES</h2>
-                    <p>USD<span> 1200</span></p>
+                    <p>USD<span> { expenses }</span></p>
                 </div>
             </div>
             <div className="balance__bottom">
                 <div className="balance__trans-info balance__trans-info--total">
-                   <h2>USD 350</h2>
+                   <h2>USD { total }</h2>
                    <p>Total Balance</p>
                 </div>
             </div>
