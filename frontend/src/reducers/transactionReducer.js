207ec953f2c00cc36588fd51796805e2ae810dd0
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-23 14:06:21 -0500

    refactor(front) refactor transaction reducer
    delete the logic to calculate incomes, expenses and total

--- frontend/src/reducers/transactionReducer.js
+++ frontend/src/reducers/transactionReducer.js
@@ -1,7 +1,4 @@
-export const initState = {
-    incomes: 0,
-    expenses: 0,
-    total: 0,
+export const initState = {   
     transactions: []
 }
 
@@ -9,20 +6,10 @@ export const transactionReducer = (state = initState, action) => {
 
     switch ( action.type ) {
         case 'ADD_TRANSACTION':
-            const total = action.payload.type === 'income'
-                            ? state.total + action.payload.amount
-                            : state.total - action.payload.amount
-            return{
+            return {
                 ...state,
-                incomes: action.payload.type === 'income' 
-                            ? state.incomes + action.payload.amount 
-                            : state.incomes,
-                expenses: action.payload.type === 'expense' 
-                ? state.expenses + action.payload.amount 
-                : state.expenses,
-                total,
-                transactions: [action.payload, ...state.transactions].splice(0,10)
-            }
+                transactions: [action.payload, ...state.transactions]
+            }    
     
         default:
             return state;
