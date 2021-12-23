9efd5063fc43670abfa8df812d57266b1ebe079c
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-23 13:38:07 -0500

    feat(front) enable search by category in history component

--- frontend/src/components/homescreen/History.js
+++ frontend/src/components/homescreen/History.js
@@ -1,9 +1,10 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 
 import CustomSelect from "./CustomSelect"
 import Transaction from "./Transaction"
 import { TransactionContext } from '../../context/transactionContext'
 import { options } from "./TransactionForm"
+import Swal from "sweetalert2"
 
 const optionsToUse=[
     { value: 'all', label: 'All items' },
@@ -14,6 +15,49 @@ const History = () => {
 
     const { state:{ transactions }} = useContext( TransactionContext );
     const [selectedOption, setSelectedOption] = useState(optionsToUse[0]);
+    const [filteredTransactions, setFilteredTransactions] = useState(null);   
+
+    useEffect(() => {
+        setFilteredTransactions(null);
+        setSelectedOption(optionsToUse[0])
+    }, [transactions])
+
+    useEffect(() => {
+
+        const getFilteredTransactions = async() => {
+
+            if( selectedOption.value === 'all' ){
+                return setFilteredTransactions(null);
+            }
+
+            try {
+                const response = await fetch(
+                    `http://localhost:4000/api/transactions/${selectedOption.value}/category`, {
+                        method: 'GET',
+                        headers:{
+                            'Content-Type': 'application/json'
+                        },
+                    }
+                )
+    
+                const filteredResult = await response.json();
+
+                if(filteredResult.errors){
+                    return Swal.fire(
+                        'Oops...', 'Please provide a valid search category', 'error'
+                    )
+                }
+
+                setFilteredTransactions( filteredResult.transactions );
+                
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getFilteredTransactions();
+
+    }, [selectedOption])
 
     return (
         <div className="history">
@@ -25,15 +69,15 @@ const History = () => {
                         <CustomSelect 
                             selectedOption={ selectedOption }
                             setSelectedOption={ setSelectedOption }
-                            options={ optionsToUse }
-                            defaultValue={ optionsToUse[0] }
+                            options={ optionsToUse }                            
                         />
                     </div>                    
                 </div>                
             </div>
             {
-                transactions.map( transaction => (
-                    <Transaction 
+                (filteredTransactions || transactions).map( transaction => (
+                    <Transaction
+                        key={ transaction.transaction_id } 
                         style={ { color: transaction.type === 'income' ? 'green': 'red' } } 
                         {...transaction}
                     />
