f795db187ef651af35ed6d7fdc621f3170c4163a
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 19:46:55 -0500

    feat(front) add new transaction feature
    create a new transaction on database

--- frontend/src/components/homescreen/TransactionForm.js
+++ frontend/src/components/homescreen/TransactionForm.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Swal from "sweetalert2";
 
+import { TransactionContext } from "../../context/transactionContext";
+import { newTransaction } from "../../actions/transactions";
 import { isValidAmount } from "../../helpers/isValidAmount";
 import  Button from '../../components/Button';
 import { useForm } from "../../hooks/useForm";
@@ -13,12 +15,13 @@ const TransactionForm = () => {
 
     const [transactionDate, setTransactionDate] = useState(new Date());
     const [selectedOption, setSelectedOption] = useState(null);
+    const { dispatch } = useContext(TransactionContext);
     const { formState:{ concept, amount }, handleInputChange, reset } = useForm({
         concept: '',
         amount: ''
     });
 
-    const handleNewTransaction = (e) => {
+    const handleNewTransaction = async(e) => {
         e.preventDefault();
 
         if( concept.trim().length === 0 ){
@@ -40,7 +43,26 @@ const TransactionForm = () => {
                 'Oops...', 'Please provide a category', 'error'
             )
         }
-        
+
+        const transaction = {
+            concept,
+            amount: value,
+            date: transactionDate,
+            category: selectedOption.value,
+            type
+        }
+
+        const response = await fetch( 'http://localhost:4000/api/transactions',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( transaction )
+        } )
+
+        const newDbTransaction = await response.json();
+
+        dispatch(newTransaction( newDbTransaction ));        
         
         setSelectedOption(null);
         setTransactionDate(new Date());
