b033d4518792a708996ad406d8cad36abbbb662d
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 20:00:38 -0500

    refactor(front) calculate data in reducer
    make calculations about total incomes, expenses and total in general

--- frontend/src/reducers/transactionReducer.js
+++ frontend/src/reducers/transactionReducer.js
@@ -9,8 +9,18 @@ export const transactionReducer = (state = initState, action) => {
 
     switch ( action.type ) {
         case 'ADD_TRANSACTION':
+            const total = action.payload.type === 'income'
+                            ? state.total + action.payload.amount
+                            : state.total - action.payload.amount
             return{
                 ...state,
+                incomes: action.payload.type === 'income' 
+                            ? state.incomes + action.payload.amount 
+                            : state.incomes,
+                expenses: action.payload.type === 'expense' 
+                ? state.expenses + action.payload.amount 
+                : state.expenses,
+                total,
                 transactions: [action.payload, ...state.transactions]
             }
     
