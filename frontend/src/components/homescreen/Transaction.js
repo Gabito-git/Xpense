8db0b63a370f1048fe4bf9ac68eb76ef4fb480b4
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 20:29:26 -0500

    refactor(front) change component prop from className to style

--- frontend/src/components/homescreen/Transaction.js
+++ frontend/src/components/homescreen/Transaction.js
@@ -2,19 +2,19 @@ import { useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import ModifyMenu from "./ModifyMenu";
 
-const Transaction = ({className}) => {
+const Transaction = ({style, concept, amount, date, category}) => {
 
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="transaction">
             <div className="transaction__left">
-                <h3 className="transaction__concept">Test transaction</h3>
-                <p className="transaction__date">Today at 12:13 PM</p>
+                <h3 className="transaction__concept">{ concept }</h3>
+                <p className="transaction__date">{ date }</p>
             </div>
             <div className="transaction__right">
-                <p>Category</p>
-                <p className={`transaction__value ${className}`}>USD <span>251</span></p>
+                <p>{ category }</p>
+                <p className="transaction__value" style={ style }>USD <span>{ amount }</span></p>
                 <BsThreeDots className="transaction__icon" onClick={ () => setIsOpen(true) }/>
             </div>
             {
