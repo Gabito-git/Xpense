7dedc29a2aeb6395140441129232c6a342af82f5
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-22 20:10:34 -0500

    (front) implement custom hook in transaction form component

--- frontend/src/components/homescreen/TransactionForm.js
+++ frontend/src/components/homescreen/TransactionForm.js
@@ -1,74 +1,25 @@
-import { useContext, useState } from "react";
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Swal from "sweetalert2";
 
-import { TransactionContext } from "../../context/transactionContext";
-import { newTransaction } from "../../actions/transactions";
-import { isValidAmount } from "../../helpers/isValidAmount";
 import  Button from '../../components/Button';
-import { useForm } from "../../hooks/useForm";
 import CustomSelect from "./CustomSelect";
+import useTransaction from "../../hooks/useTransaction";
 
 const TransactionForm = () => {
 
-    const [transactionDate, setTransactionDate] = useState(new Date());
-    const [selectedOption, setSelectedOption] = useState(null);
-    const { dispatch } = useContext(TransactionContext);
-    const { formState:{ concept, amount }, handleInputChange, reset } = useForm({
-        concept: '',
-        amount: ''
-    });
-
-    const handleNewTransaction = async(e) => {
-        e.preventDefault();
-
-        if( concept.trim().length === 0 ){
-            return Swal.fire(
-                'Oops...', 'Please provide a valid concept', 'error'
-            )
-        } 
+    const {
+        transactionDate,
+        setTransactionDate,
+        selectedOption,
+        setSelectedOption,
+        concept, 
+        amount,
         
-        const { ok, value, type } = isValidAmount( amount );
-
-        if( !ok ){
-            return Swal.fire(
-                'Oops...', 'Please provide a valid amount', 'error'
-            )
-        }
-
-        if( selectedOption === null){
-            return Swal.fire(
-                'Oops...', 'Please provide a category', 'error'
-            )
-        }
-
-        const transaction = {
-            concept,
-            amount: value,
-            date: transactionDate,
-            category: selectedOption.value,
-            type
-        }
-
-        const response = await fetch( 'http://localhost:4000/api/transactions',{
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify( transaction )
-        } )
-
-        const newDbTransaction = await response.json();
-
-        dispatch(newTransaction( newDbTransaction ));        
-        
-        setSelectedOption(null);
-        setTransactionDate(new Date());
-        reset();
-    }
-
+        handleInputChange,
+        handleNewTransaction
+    } = useTransaction();
+    
     return (
         <div className="transaction-form">
             <h2 className="transaction-form__title"> New Transaction </h2>
