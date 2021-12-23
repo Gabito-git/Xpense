0dcf4ffd2ee80c8de3598eead5746ba74d98add0
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-23 14:04:26 -0500

    (front) add action to delete a transaction

--- frontend/src/actions/transactions.js
+++ frontend/src/actions/transactions.js
@@ -2,3 +2,8 @@ export const newTransaction = ( transaction ) => ({
     type: 'ADD_TRANSACTION',
     payload: transaction
 })
+
+export const deleteTransaction = ( id ) => ({
+    type: 'DEL_TRANSACTION',
+    payload: id
+})
