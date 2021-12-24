d9f2aea53b5a59490d90881e8cec94e91490c9fe
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-24 10:13:39 -0500

    refactor(front) modify isValidAmount fucntion
    perform modification to check if the object toModify is set

--- frontend/src/helpers/isValidAmount.js
+++ frontend/src/helpers/isValidAmount.js
@@ -1,7 +1,19 @@
-export const isValidAmount = ( amount ) => {
+export const isValidAmount = ( amount, flag ) => {
+    console.log(amount)
+
+    if( flag && amount < 0 ){
+        return {
+            ok: false,
+            message: 'Just type the value, you can not change the type of transaction'
+        };
+    }
+
     
     if( isNaN(amount) || amount.trim().length === 0 ){
-        return {ok: false};
+        return {
+            ok: false,
+            message: 'Please provide a valid amount'
+        };
     }
 
     return {
