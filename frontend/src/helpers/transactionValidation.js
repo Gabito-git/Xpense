7ed2c90cf113e6a466e53fb8ce491f2b8a81c276
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-24 16:08:55 -0500

    refactor(front) create validation file for transaction fields

---
+++ frontend/src/helpers/transactionValidation.js
@@ -0,0 +1,22 @@
+const transactionValidation = ( values ) => {
+
+    const { concept, amount, category } = values;
+
+    const errors = {}
+    
+    if( !concept.trim() ){
+        errors.concept = 'Required'
+    }
+
+    if( isNaN(amount) || !amount.trim() ){
+        errors.amount = 'Please provide a valid amount'
+    }
+
+    if( !category ){
+        errors.category = 'Required'
+    }
+
+    return errors;
+}
+
+export default transactionValidation
