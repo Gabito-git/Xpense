497def80d49d42f81fd41aa0f4e04ffb75c037d3
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-27 14:34:20 -0500

    feat(backend) generate jwt and attach to cookie

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,34 +1,59 @@
 const bcrypt = require("bcryptjs/dist/bcrypt");
+const jwt = require('jsonwebtoken');
 
 const { pool } = require("../db/config");
 const BadRequestError = require("../errors/bad-request-err");
 
 const signUp = async( req, res ) => {
     const { username, email, password } = req.body;
-    // console.log(username, email, password)
+    let response;
 
     try {
-        const response = await pool.query(
+         response = await pool.query(
             "SELECT * FROM users WHERE email = $1", 
             [email]
         );
+    }catch(error){
+        console.log(error)
+    }
 
-        if( response.rowCount > 0 ){
-            throw new BadRequestError('Email in use');
-        }else{
-            const salt           = bcrypt.genSaltSync();
-            const hashedPassword = bcrypt.hashSync( password, salt );
-            const user = await pool.query(
-                "INSERT INTO users (username, email, password) "+
-                "VALUES($1, $2, $3) RETURNING username, user_id", [username, email, hashedPassword]
-            )
-            res.send( user.rows )
-        }
+    if( response.rowCount > 0 ){
+        throw new BadRequestError('Email in use');
+    }
 
+    try{         
+
+        const salt           = bcrypt.genSaltSync();
+        const hashedPassword = bcrypt.hashSync( password, salt );
+        let user = await pool.query(
+            "INSERT INTO users (username, email, password) "+
+            "VALUES($1, $2, $3) RETURNING username, user_id", 
+            [username, email, hashedPassword]
+        )
+
+        user = user.rows[0];
+
+        const userJwt = jwt.sign({
+                id: user.user_id,
+                username: user.username
+            }, process.env.JWT_KEY,{
+                expiresIn: '2h'
+            }        
+        )    
+        // Store on Session object
+        req.session.jwt = { userJwt }               
         
-
+        res.send( user )
+        
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).json(
+            {   
+                errors: [{
+                    message: 'Please contact the admin'
+                }] 
+            }
+        )
     }
 }
 
