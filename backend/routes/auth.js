84fae2f56c156abb3fe0822bd9b43a61d0bfcf7c
Gabriel <luisgabrielarevalo@hotmail.com>
2021-12-27 14:40:06 -0500

    feat(backend) add validation to signup route handler

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,18 @@
 
 const { Router } = require('express');
+const { check }  = require('express-validator'); 
+
 const { signUp } = require('../controllers/auth');
+const validateFields = require('../middlewares/validateFields');
 
 const router = Router();
 
-router.post('/signup', signUp)
+router.post('/signup',[
+    check('username', 'Username field is mandatory').not().isEmpty(),
+    check('email', 'Please provide a valid e-mail').isEmail(),
+    check('password', 'Password field is mandatory').not().isEmpty(),
+    validateFields
+ ], signUp)
 
 
 module.exports = router;
