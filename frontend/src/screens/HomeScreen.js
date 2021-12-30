7f3c0203ef78848078eb67449cc1da8e7b3ceea9
LUIS AREVALO <luisgabriel@MacBook-Air.local>
2021-12-30 18:21:57 -0500

    (front) add navbar component to homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,17 +2,15 @@ import Button from "../components/Button"
 import Balance from "../components/homescreen/Balance"
 import History from "../components/homescreen/History"
 import TransactionForm from "../components/homescreen/TransactionForm"
+import Navbar from "../components/Navbar"
 
 const HomeScreen = () => {
     return (
         <div className="homescreen">
-            <nav className="homescreen__navbar">
-                <h1>XPENSE</h1>    
-                <Button 
-                    text="Logout" 
-                    className="homescreen__nav-button"
-                />            
-            </nav>
+            <Navbar 
+                className="homescreen"
+                buttonText="Logout"
+            />
 
             <main className="homescreen__main">
                 <div className="homescreen__left">
