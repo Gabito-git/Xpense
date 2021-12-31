1c226e1a798e5c3b1b832c357c5502da3c7088c9
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2021-12-31 18:33:09 -0500

    (front) use money asset in signup screen component

--- frontend/src/screens/SingupScreen.js
+++ frontend/src/screens/SingupScreen.js
@@ -1,6 +1,7 @@
 import AuthForm from "../components/authscreen/AuthForm"
 import FormGroup from "../components/authscreen/FormGroup"
 import Navbar from "../components/Navbar"
+import money from '../assets/money.png'
 
 const SingupScreen = () => {
     return (
@@ -31,6 +32,9 @@ const SingupScreen = () => {
                     />
                 </AuthForm>
                 <div className="auth__bottom"></div>
+                <div className="auth__money animate__animated animate__rotateIn">
+                    <img src={ money } alt="money" />
+                </div>
             </main>
         </div>
     )
