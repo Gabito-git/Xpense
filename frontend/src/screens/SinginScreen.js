b6a9180c104c1f65b2a97383d5ee6cf0e89dbec9
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-01 11:30:22 -0500

    feat(front) add singin screen markup and classes

--- frontend/src/screens/SinginScreen.js
+++ frontend/src/screens/SinginScreen.js
@@ -1,8 +1,38 @@
+import AuthForm from "../components/authscreen/AuthForm"
+import FormGroup from "../components/authscreen/FormGroup"
+import Navbar from "../components/Navbar"
+import money from '../assets/money.png'
+
 const SinginScreen = () => {
     return (
-        <h1>
-            SigninScreen
-        </h1>
+        <div className="auth">
+            <Navbar 
+                buttonText="Sign up"
+                className="auth"
+            />
+            <main className="auth__main">
+                <h1 className="auth__title">Experience a fresh way to manage money</h1>
+                <AuthForm
+                    className="auth" 
+                    buttonText="Sign in"
+                >
+                    <FormGroup
+                        className="auth"
+                        label="Email"
+                        type="email"
+                    />
+                    <FormGroup 
+                        className="auth"
+                        label="Password"
+                        type="password"
+                    />
+                </AuthForm>
+                <div className="auth__bottom"></div>
+                <div className="auth__money animate__animated animate__rotateIn">
+                    <img src={ money } alt="money" />
+                </div>
+            </main>
+        </div>
     )
 }
 
