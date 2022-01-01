b3862bff1c4c9b29719c6d02abcebb594c675868
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-01 15:00:12 -0500

    feat(front) implment form context in form group component

--- frontend/src/components/authscreen/FormGroup.js
+++ frontend/src/components/authscreen/FormGroup.js
@@ -1,4 +1,10 @@
-const FormGroup = ({ className, label, type }) => {
+import { useContext } from "react"
+import { FormContext } from "./AuthForm"
+
+const FormGroup = ({ className, label, type, name }) => {
+
+    const { functions:{ handleInputChange }, values } = useContext(FormContext);
+    
     return (
         <div className={ `${ className }__form-group` }>
             <label className={ `${ className }__label` }>{ label }</label>
@@ -6,6 +12,9 @@ const FormGroup = ({ className, label, type }) => {
                 <input 
                     className={ `${ className }__input` } 
                     type={ type }
+                    name={ name }
+                    value={ values[name] }
+                    onChange={ handleInputChange }
                 />
             </div>
         </div>
