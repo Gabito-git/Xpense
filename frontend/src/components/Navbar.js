d50bbe3a8b676d3a9a7b8251ffb953f68e72ad8c
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-01 11:44:58 -0500

    feat(front) add onclicktitle function in navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import Button from "./Button"
 
-const Navbar = ({ className, buttonText }) => {
+const Navbar = ({ className, buttonText, onTitleClick }) => {
     return (
         <nav className={`${ className }__navbar`}>
-            <h1>XPENSE</h1>    
+            <h1 
+                onClick={ onTitleClick }
+                style={ { cursor: `${ onTitleClick ? 'pointer': ' default' }` } }
+            >XPENSE</h1>    
             <Button
                 text={ buttonText } 
                 className={`${ className }__nav-button`}
