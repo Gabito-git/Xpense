34b5cb48210fdc5a1326881e5fb155a18d02dbfe
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-02 13:07:57 -0500

    feat(front) add signout function to button in homscreen component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,33 @@
+import { useContext } from "react"
+import { setCurrentUser } from "../actions/auth"
+
 import Button from "../components/Button"
 import Balance from "../components/homescreen/Balance"
 import History from "../components/homescreen/History"
 import TransactionForm from "../components/homescreen/TransactionForm"
 import Navbar from "../components/Navbar"
+import { AuthContext } from "../context/authContext"
+import fetchHelper from "../helpers/fetchHelper"
 
 const HomeScreen = () => {
+
+    const { dispatch } = useContext(AuthContext)
+
+    const handleSignOut = async() => {
+        await fetchHelper({
+            url:'users/signout',
+            method: 'get'
+        })
+
+        dispatch( setCurrentUser( null ) );
+    }
+
     return (
         <div className="homescreen">
             <Navbar 
                 className="homescreen"
                 buttonText="Logout"
+                onButtonClick={ handleSignOut }
             />
 
             <main className="homescreen__main">
