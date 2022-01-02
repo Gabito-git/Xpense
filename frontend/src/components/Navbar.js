da45245e85ec96aa0b1e737ed0021bdab384c6db
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-02 14:17:19 -0500

    refactor(front) add username div to navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,31 @@
 import Button from "./Button"
 
-const Navbar = ({ className, buttonText, onTitleClick, onButtonClick }) => {
+const Navbar = ({ className, buttonText, onTitleClick, onButtonClick, username=null}) => {
     return (
         <nav className={`${ className }__navbar`}>
             <h1 
                 onClick={ onTitleClick }
                 style={ { cursor: `${ onTitleClick ? 'pointer': ' default' }` } }
             >XPENSE</h1>    
-            <Button
-                text={ buttonText } 
-                className={`${ className }__nav-button`}
-                onClick={ onButtonClick }
-            />            
+            
+            <div className={`${ className }__nav-right`}>
+                {
+                    username && (
+                        <div className={`${ className }__nav-username`}>
+                            <h3>{ username }</h3>
+                        </div>  
+                    )
+                }
+
+                <Button
+                    text={ buttonText } 
+                    className={`${ className }__nav-button`}
+                    onClick={ onButtonClick }
+                />  
+    
+            </div>
+            
+                    
         </nav>
     )
 }
