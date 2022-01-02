ed06d0b744d5ed2c280442f4bcde6d957d475372
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-02 12:51:35 -0500

    feat(front) add getCurrentUser function to AppRouter

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -1,12 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import {
     Switch,
     Route,
     Redirect,
     BrowserRouter as Router
 } from 'react-router-dom';
+import { finishChecking, setCurrentUser } from '../actions/auth';
 
 import { AuthContext } from '../context/authContext';
+import fetchHelper from '../helpers/fetchHelper';
 import PrivateRoute from '../routes/PrivateRoute';
 import PublicRoute from '../routes/PublicRoutes';
 import HomeScreen from '../screens/HomeScreen';
@@ -16,6 +18,25 @@ const AppRouter = () => {
 
     const { state, dispatch } = useContext( AuthContext );
     const { checking, currentUser } = state;
+
+    useEffect(() => {
+       const getCurrentUser = async() => {
+           const response = await fetchHelper({
+               url:'users/currentuser',
+               method: 'get',
+           })
+
+           const currentUser = await response.json();
+           dispatch(setCurrentUser( currentUser.currentUser ))
+           dispatch( finishChecking() )
+       }
+
+       getCurrentUser();
+    }, [])
+
+    if( state.checking ){
+        return <h1>....Checking</h1>
+    }
 
     return (
         <Router>
